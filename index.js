5943f7613683489fd13202f0ab830e24fdd9edd8
Anton <Iarovoi>
2022-02-04 15:09:46 +0200

    init deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 const cors = require('cors')
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 const dbUrl = process.env.DATABASE_URL
 
 app.use(express.json())
