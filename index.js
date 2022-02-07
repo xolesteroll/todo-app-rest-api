65ea2e48e61f240689709a049ba86f3880c41d4f
Anton <Iarovoi>
2022-02-07 08:46:59 +0200

    debug

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000
 const dbUrl = process.env.DATABASE_URL
 
+console.log(dbUrl, PORT)
+
 app.use(express.json())
 app.use(cors())
 app.use('/api/auth', authRouter)
